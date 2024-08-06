825ef4775f7b7e3ea08f9fd34880d89c0c55de20
Rohtash Sethi <rohtash.sethi@gmail.com>
2024-08-07 00:45:56 +0530

    21 security improvements (#22)
    * deleted .env file
    
    * securely access client secret

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OauthModule } from './oauth/oauth.module';
 
 @Module({
-  imports: [OauthModule],
+  imports: [
+    OauthModule,
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the ConfigModule globally available
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
